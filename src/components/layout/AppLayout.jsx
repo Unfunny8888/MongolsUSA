db377e142d7c091218c37620b38d9c307866670e
Dee Dashka <dashka6539@gmail.com>
2026-05-08 11:11:19 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,10 +1,9 @@
-import { Outlet, useNavigate, useLocation } from "react-router-dom";
-import { useEffect, useCallback } from "react";
-import { useSwipeGesture } from "../../hooks/useSwipeGesture";
+import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 import BottomNav from "./BottomNav";
 
 
-const NAV_ORDER = ["/", "/search", "/explore", "/groups", "/profile"];
+
 
 function registerSW() {
   if ("serviceWorker" in navigator) {
@@ -15,31 +14,10 @@ function registerSW() {
 }
 
 export default function AppLayout() {
-  const navigate = useNavigate();
-  const location = useLocation();
-
   useEffect(() => { registerSW(); }, []);
 
-  const currentIdx = NAV_ORDER.indexOf(location.pathname);
-
-  const onSwipeLeft = useCallback(() => {
-    if (currentIdx >= 0 && currentIdx < NAV_ORDER.length - 1) {
-      navigate(NAV_ORDER[currentIdx + 1]);
-    }
-  }, [currentIdx, navigate]);
-
-  const onSwipeRight = useCallback(() => {
-    if (currentIdx > 0) {
-      navigate(NAV_ORDER[currentIdx - 1]);
-    } else if (currentIdx === -1) {
-      navigate(-1);
-    }
-  }, [currentIdx, navigate]);
-
-  const swipeRef = useSwipeGesture(onSwipeLeft, onSwipeRight, { threshold: 80 });
-
   return (
-    <div ref={swipeRef} className="app-container bg-background">
+    <div className="app-container bg-background">
       <main className="pb-24 max-w-lg mx-auto">
         <Outlet />
       </main>
